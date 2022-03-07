3679e3896746cac3505acac11e4b68d7b9e27e22
NVG-64 <aryslow92@gmail.com>
2022-03-07 09:39:05 +0700

    my checkpoint in exercise 1

---
+++ exercise/1/exercise.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>Num.</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td><img src="img/mace_2.jpg" width="100"></td>
+      <td>11111111</td>
+      <td>Mace</td>
+      <td>mace@example.mail</td>
+      <td>Operator Jackals</td>
+      <td>
+        <a href="">Ubah</a> | <a href="">Hapus</a>
+      </td>
+    </tr>
+  </table>
+
+</body>
+
+</html>
+//4.55
